7ecf0fd61fe0a5f37315f5ace0d4c24a9ae49273
Dois2 <silveira.lsv@gmail.com>
2022-01-02 13:13:24 -0300

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import { About } from '../components/about'
 import { Footer } from '../components/footer'
 
 import { HomeSection } from '../components/home'
+import { Projects } from '../components/projectCard'
 
 const Home: NextPage = () => {
   return (
     <main >
       <HomeSection />
       <About />
+      <Projects/>
       <Footer/>
     </main>
   )
