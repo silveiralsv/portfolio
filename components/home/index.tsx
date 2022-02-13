a64b66fd0da7c32080fc38db5be178cbe1782703
Dois2 <silveira.lsv@gmail.com>
2022-02-13 20:42:43 -0300

    Added Padding on mobile

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Image from 'next/image'
-
-import hero from '../assets/hero.svg'
 import home from '../assets/home.svg'
 import { ContactBtn } from '../ContactBtn';
 import { FaGithub, FaLinkedin, FaMailBulk } from 'react-icons/fa';
@@ -10,10 +8,10 @@ import { FiMail } from 'react-icons/fi';
 
 export const HomeSection: React.FC = () => {
   return (
-  <div className=" snap-start w-screen h-screen flex flex-col md:flex-row justify-between ">
-    <div className='flex h-1/2 md:h-screen w-fit  py-5'>
-      <Image src={home} alt='home' />
-    </div>
+  <div className="flex flex-col md:flex-row items-center justify-between p-8">
+    <span className='flex max-h-[90vh]'>
+      <Image src={home} alt='home'/>
+    </span>
 
 
     <span className=" grow text-center my-auto text-5xl text-gray-300">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,16 @@
 import type { NextPage } from 'next'
 import { About } from '../components/about'
-import { Footer } from '../components/footer'
-
 import { HomeSection } from '../components/home'
-import { Projects } from '../components/projectCard'
 
 const Home: NextPage = () => {
   return (
-    <main className='max-h-screen bg-black text-slate-100 scroll-smooth font-sans snap snap-y overflow-y-scroll snap-mandatory'>
-      
-      <HomeSection />
-
-      
-      <About />
-
-      
-
-      
-
-
+    <main className='max-h-screen h-screen scroll-smooth bg-black text-slate-100 font-sans snap snap-y overflow-y-scroll overflow-x-hidden snap-mandatory'>
+      <div className="snap-center h-screen">
+        <HomeSection />
+      </div>
+      <div className="snap-center h-screen">
+        <About />
+      </div>
     </main>
   )
 }
