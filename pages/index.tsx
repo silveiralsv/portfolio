ce9506dceec25e23d5bf8d933061fb1245aa7532
silveiralsv <silveira.lsv@gmail.com>
2022-02-14 11:10:55 -0300

    and this?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 import { About } from '../components/about'
 import { HomeSection } from '../components/home'
 
 const Home: NextPage = () => {
+
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+  }, [])
+
   return (
-    <main className='absolute inset-0 max-h-screen scroll-smooth bg-black text-slate-100 font-sans snap snap-y overflow-y-scroll overflow-x-hidden snap-mandatory'>
-      <div className="snap-start h-full">
+    <main className='max-h-screen scroll-smooth bg-black text-slate-100 font-sans snap snap-y overflow-y-scroll overflow-x-hidden snap-mandatory'>
+      <div className="snap-start h-screen">
         <HomeSection />
       </div>
-      <div className="snap-start h-full">
+      <div className="snap-start h-screen">
         <About />
       </div>
     </main>
